55dcbc6ecfbc5be750bceff8edde257ad917c6c6
Rachel Chen <rachel@chens.email>
2022-07-26 20:58:08 -0700

    initial commit

---
+++ cloudflare.go
@@ -0,0 +1,86 @@
+package challenger
+
+import (
+	"kon.nect.sh/challenger/cloudflare"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+)
+
+// Provider wraps the provider implementation as a Caddy module.
+type Provider struct{ *cloudflare.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.challenger",
+		New: func() caddy.Module { return &Provider{new(cloudflare.Provider)} },
+	}
+}
+
+// Before using the provider config, resolve placeholders in the API token and RootZone
+// Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	r := caddy.NewReplacer()
+	p.Provider.APIToken = r.ReplaceAll(p.Provider.APIToken, "")
+	p.Provider.RootZone = r.ReplaceAll(p.Provider.RootZone, "")
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+// cloudflare [<api_token>] {
+//     api_token <api_token>
+//     root_zone <zone_hosting_challenges>
+// }
+//
+// Expansion of placeholders in the API token is left to the JSON config caddy.Provisioner (above).
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			p.Provider.APIToken = d.Val()
+		}
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_token":
+				if p.Provider.APIToken != "" {
+					return d.Err("API token already set")
+				}
+				p.Provider.APIToken = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "root_zone":
+				if p.Provider.RootZone != "" {
+					return d.Err("Root Zone already set")
+				}
+				p.Provider.RootZone = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.APIToken == "" {
+		return d.Err("missing API token")
+	}
+	if p.Provider.RootZone == "" {
+		return d.Err("missing root zone")
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
---
+++ cloudflare/provider.go
@@ -0,0 +1,124 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/libdns/libdns"
+)
+
+// Provider implements the libdns interfaces for Cloudflare.
+// TODO: Support pagination and retries, handle rate limits.
+type Provider struct {
+	// API token is used for authentication. Make sure to use a
+	// scoped API **token**, NOT a global API **key**. It will
+	// need two permissions: Zone-Zone-Read and Zone-DNS-Edit,
+	// unless you are only using `GetRecords()`, in which case
+	// the second can be changed to Read.
+	APIToken string `json:"api_token,omitempty"`
+	// Root Zone points to the actual hosted zone for the TXT records.
+	// If you are hosting secure.example.com's acme challenge, then
+	// _acme-challenge.secure.example.com should be CNAME to
+	// secure.example.com.{RootZone}.
+	RootZone string `json:"root_zone,omitempty"`
+
+	zoneCache *cfZone
+	zoneMu    sync.Mutex
+}
+
+const (
+	stripToken = "_acme-challenger."
+	slen       = len(stripToken)
+)
+
+func (p *Provider) strip(name string) string {
+	if strings.HasSuffix(name, stripToken) {
+		return name[slen:]
+	}
+	return name
+}
+
+// AppendRecords adds records to the zone. It returns the records that were added.
+func (p *Provider) AppendRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	zoneInfo, err := p.getZoneInfo(ctx, p.RootZone)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%+v\n", records)
+
+	var created []libdns.Record
+	for _, rec := range records {
+		rec.Name = p.strip(rec.Name)
+		result, err := p.createRecord(ctx, zoneInfo, rec)
+		if err != nil {
+			return nil, err
+		}
+		created = append(created, result.libdnsRecord(zone))
+	}
+
+	return created, nil
+}
+
+// DeleteRecords deletes the records from the zone. If a record does not have an ID,
+// it will be looked up. It returns the records that were deleted.
+func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []libdns.Record) ([]libdns.Record, error) {
+	zoneInfo, err := p.getZoneInfo(ctx, zone)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("%+v\n", records)
+
+	var recs []libdns.Record
+	for _, rec := range records {
+		// we create a "delete queue" for each record
+		// requested for deletion; if the record ID
+		// is known, that is the only one to fill the
+		// queue, but if it's not known, we try to find
+		// a match theoretically there could be more
+		// than one
+		var deleteQueue []libdns.Record
+
+		if rec.ID == "" {
+			// record ID is required; try to find it with what was provided
+			exactMatches, err := p.getDNSRecords(ctx, zoneInfo, rec, true)
+			if err != nil {
+				return nil, err
+			}
+			for _, rec := range exactMatches {
+				deleteQueue = append(deleteQueue, rec.libdnsRecord(zone))
+			}
+		} else {
+			deleteQueue = []libdns.Record{rec}
+		}
+
+		for _, delRec := range deleteQueue {
+			reqURL := fmt.Sprintf("%s/zones/%s/dns_records/%s", baseURL, zoneInfo.ID, delRec.ID)
+			req, err := http.NewRequestWithContext(ctx, "DELETE", reqURL, nil)
+			if err != nil {
+				return nil, err
+			}
+
+			var result cfDNSRecord
+			_, err = p.doAPIRequest(req, &result)
+			if err != nil {
+				return nil, err
+			}
+
+			recs = append(recs, result.libdnsRecord(zone))
+		}
+
+	}
+
+	return recs, nil
+}
+
+// Interface guards
+var (
+	_ libdns.RecordAppender = (*Provider)(nil)
+	_ libdns.RecordDeleter  = (*Provider)(nil)
+)
